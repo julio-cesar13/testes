dc24f060312884ff04b5be99e68f839727fb8eac
julio-cesar13 <120072953+julio-cesar13@users.noreply.github.com>
2023-01-14 19:58:26 -0400

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,60 +1,113 @@
 import pygame
+import math
 from draw_screen import draw_object
-from move import move
 
 pygame.init()
 pygame.font.init()
 
-size = (1000, 750)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Breakout Game")
+# Screen
+sc_width = 1000
+sc_height = 750
+screen = pygame.display.set_mode((sc_width, sc_height))
+border = 25
 screen.fill("#9f4100")
+pygame.display.set_caption("Breakout Game")
+font = pygame.font.Font("PressStart2P.ttf", 40)
 
-font = pygame.font.Font("PressStart2P.ttf", 30)
-
+# Score (player 1)
 score_point_player1 = font.render("0", True, (0, 255, 0))
 score_point_player1_rect = score_point_player1.get_rect()
-score_point_player1_rect.center = (350, 30)
-
+score_point_player1_rect.center = (sc_width / 4, 25)
+# Score (player 2)
 score_point_player2 = font.render("0", True, (0, 0, 255))
 score_point_player2_rect = score_point_player2.get_rect()
-score_point_player2_rect.center = (650, 30)
+score_point_player2_rect.center = (3 * sc_width / 4, 25)
 
+# Load images
 player1 = pygame.sprite.Sprite()
-player1.image = pygame.image.load("player1.png")
-player1.image = pygame.transform.scale(player1.image, (50, 50))
-player1.image = pygame.transform.rotate(player1.image, 180)
-player1.rect = pygame.Rect(910, 380, 0, 0)
-
-angle = 0
-
 player2 = pygame.sprite.Sprite()
-player2.image = pygame.image.load("player2.png")
-player2.image = pygame.transform.scale(player2.image, (50, 50))
-player2.rect = pygame.Rect(50, 380, 0, 0)
+
+
+def load_images(sprite, ang, xp, yp, image):
+    sprite.image = pygame.image.load(image)
+    sprite.image = pygame.transform.scale(sprite.image, (50, 50))
+    sprite.image = pygame.transform.rotate(sprite.image, ang)
+    cor_x = sprite.image.get_rect(center=(xp + 25, yp + 25))
+    sprite.rect = cor_x
+    return cor_x
+
+
+xp1 = 50
+yp1 = 380
+xp2 = sc_width - 100
+yp2 = 380
+radius = 25 * (2 ** 0.5)
+
+ang_left = 0
+ang_right = 0
 
 clock = pygame.time.Clock()
-
+per_1 = False
+per_2 = False
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    screen.fill("#9f4100")
+    keys = pygame.key.get_pressed()
+    blue = (0, 0, 255)
 
-    a = move(player1, angle)[0]
-    b = move(player1, angle)[1]
-    c = move(player1, angle)[2]
-    angle = c
-    screen.blit(a, b)
+    player1_cor = load_images(player1, ang_left, xp1, yp1, "player1.png")
+    player2_cor = load_images(player2, ang_right, xp2, yp2, "player2.png")
+    player2.image = pygame.transform.rotate(player2.image, 180)
 
-    draw_object(screen)
+    # collision player 1 with objects
+    collision_1 = (
+        25 + xp1 + 25 * math.sin(math.radians(ang_left + 90)), yp1 + 25 * math.cos(math.radians(ang_left + 90)), 5,
+        50)
+    if draw_object(screen, collision_1):
+        per_1 = True
+    else:
+        per_1 = False
 
-    screen.blit(player2.image, player2.rect)
+    # collision player 2 with objects
+    collision_2 = (
+        25 + xp2 - 25 * math.sin(math.radians(ang_right + 90)), yp2 - 25 * math.cos(math.radians(ang_right + 90)), 5,
+        50)
+    if draw_object(screen, collision_2):
+        per_2 = True
+    else:
+        per_2 = False
+
+    if keys[pygame.K_UP] and per_1:
+        xp1 += math.sin(math.radians(ang_left + 90))
+        yp1 += math.cos(math.radians(ang_left + 90))
+
+    elif keys[pygame.K_LEFT]:
+        ang_left += 1
+
+    elif keys[pygame.K_RIGHT]:
+        ang_left += -1
+
+    if keys[pygame.K_w] and per_2:
+        xp2 += math.sin(math.radians(ang_right - 90))
+        yp2 += math.cos(math.radians(ang_right - 90))
+
+    elif keys[pygame.K_a]:
+        ang_right += 1
+
+    elif keys[pygame.K_d]:
+        ang_right += -1
+
+    screen.blit(player1.image, player1_cor)
+    screen.blit(player2.image, player2_cor)
 
     screen.blit(score_point_player1, score_point_player1_rect)
     screen.blit(score_point_player2, score_point_player2_rect)
 
+    draw_object(screen, player1.rect)
+
     pygame.display.flip()
-    clock.tick(50)
+    screen.fill("#9f4100")
+    clock.tick(150)
