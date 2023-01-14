b7e4ddeddf12ec771ab142c6eb57bfc7e32bd2dd
julio-cesar13 <120072953+julio-cesar13@users.noreply.github.com>
2023-01-13 21:09:03 -0400

    Update move.py

--- move.py
+++ move.py
@@ -10,6 +10,7 @@ def move(player1, angle):
         new_position = rotated_image.get_rect(
             center=(player1.rect.x + 25, player1.rect.y + 25))
         return rotated_image, new_position, angle
+
     elif keys[pygame.K_RIGHT]:
         angle -= 1
         rotated_image = pygame.transform.rotate(player1.image, angle)
@@ -17,18 +18,6 @@ def move(player1, angle):
             center=(player1.rect.x + 25, player1.rect.y + 25))
         return rotated_image, new_position, angle
 
-    if keys[pygame.K_a]:
-        angle += 1
-        rotated_image = pygame.transform.rotate(player1.image, angle)
-        new_position = rotated_image.get_rect(
-            center=(player1.rect.x + 25, player1.rect.y + 25))
-        return rotated_image, new_position, angle
-    elif keys[pygame.K_RIGHT]:
-        angle -= 1
-        rotated_image = pygame.transform.rotate(player1.image, angle)
-        new_position = rotated_image.get_rect(
-            center=(player1.rect.x + 25, player1.rect.y + 25))
-        return rotated_image, new_position, angle
     else:
         rotated_image = pygame.transform.rotate(player1.image, angle)
         new_position = rotated_image.get_rect(
