5c6dae50fffd7a8d6ada851ef434d65ff7b42b81
julio-cesar13 <120072953+julio-cesar13@users.noreply.github.com>
2023-01-14 23:33:11 -0400

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,7 +5,10 @@ from draw_screen import draw_object
 pygame.init()
 pygame.font.init()
 
+# color balls
 green = (0, 255, 0)
+blue = (0, 0, 255)
+
 # score´s
 score1 = 0
 score2 = 0
@@ -28,12 +31,18 @@ score_point_player2 = font.render("0", True, (0, 0, 255))
 score_point_player2_rect = score_point_player2.get_rect()
 score_point_player2_rect.center = (3 * sc_width / 4, 25)
 
-# ball of left tank
+# ball from left tank
 ball_x_left = -5
 ball_y_left = -5
 ball_dx_left = 0
 ball_dy_left = 0
 ball_left = pygame.draw.rect(screen, green, (ball_x_left, ball_y_left, 5, 5))
+# ball from right tank
+ball_x_right = -5
+ball_y_right = -5
+ball_dx_right = 0
+ball_dy_right = 0
+ball_right = pygame.draw.rect(screen, green, (ball_x_right, ball_y_right, 5, 5))
 
 # Load images
 player1 = pygame.sprite.Sprite()
@@ -93,33 +102,35 @@ while game_loop:
     else:
         per_2 = False
 
-    if keys[pygame.K_UP] and per_1:
+    if keys[pygame.K_w] and per_1:
         xp1 += math.sin(math.radians(ang_left + 90))
         yp1 += math.cos(math.radians(ang_left + 90))
 
-    elif keys[pygame.K_LEFT]:
+    elif keys[pygame.K_a]:
         ang_left += 1
 
-    elif keys[pygame.K_RIGHT]:
+    elif keys[pygame.K_d]:
         ang_left += -1
 
-    if keys[pygame.K_w] and per_2:
+    if keys[pygame.K_UP] and per_2:
         xp2 += math.sin(math.radians(ang_right - 90))
         yp2 += math.cos(math.radians(ang_right - 90))
 
-    elif keys[pygame.K_a]:
+    elif keys[pygame.K_LEFT]:
         ang_right += 1
 
-    elif keys[pygame.K_d]:
+    elif keys[pygame.K_RIGHT]:
         ang_right += -1
 
-    if keys[pygame.K_KP_ENTER]:
+    # start player 1 ball
+    if keys[pygame.K_q]:
         ball_x_left = 25 + xp1 + 25 * math.sin(math.radians(ang_left + 90))
         ball_y_left = 25 + yp1 + 25 * math.cos(math.radians(ang_left + 90))
         ball_left = pygame.draw.rect(screen, green, (ball_x_left, ball_y_left, 5, 5))
         ball_dx_left = math.sin(math.radians(ang_left + 90))
         ball_dy_left = math.cos(math.radians(ang_left + 90))
 
+    # check ball collisions with objects
     if not draw_object(screen, ball_left):
         ball_dy_left *= -1
         ball_x_left = ball_x_left + ball_dx_left
@@ -127,21 +138,48 @@ while game_loop:
         ball_left = pygame.draw.rect(screen, green, (ball_x_left, ball_y_left, 5, 5))
         if not draw_object(screen, ball_left):
             ball_dx_left *= -1
-
+    # check ball collision with player 1
     if ball_left.colliderect(player2):
         score1 += 1
         score_point_player1 = font.render(str(score1), True, (0, 255, 0))
-        continue
+
+    # start player 1 ball
+    if keys[pygame.K_SEMICOLON]:
+        ball_x_right = 25 + xp2 - 25 * math.sin(math.radians(ang_right + 90))
+        ball_y_right = 25 + yp2 - 25 * math.cos(math.radians(ang_right + 90))
+        ball_right = pygame.draw.rect(screen, blue, (ball_x_right, ball_y_right, 5, 5))
+        ball_dx_right = math.sin(math.radians(ang_right - 90))
+        ball_dy_right = math.cos(math.radians(ang_right - 90))
+
+    # check ball collisions with objects
+    if not draw_object(screen, ball_right):
+        ball_dy_right *= -1
+        ball_x_right = ball_x_right + ball_dx_right
+        ball_y_right = ball_y_right + ball_dy_right
+        ball_right = pygame.draw.rect(screen, blue, (ball_x_right, ball_y_right, 5, 5))
+        if not draw_object(screen, ball_right):
+            ball_dx_right *= -1
+
+    # check ball collision with player 1
+    if ball_right.colliderect(player1):
+        score2 += 1
+        score_point_player2 = font.render(str(score2), True, (0, 0, 255))
 
     ball_x_left = ball_x_left + ball_dx_left
     ball_y_left = ball_y_left + ball_dy_left
+
+    ball_x_right = ball_x_right + ball_dx_right
+    ball_y_right = ball_y_right + ball_dy_right
 
     screen.blit(player1.image, player1_cor)
     screen.blit(player2.image, player2_cor)
 
     screen.blit(score_point_player1, score_point_player1_rect)
     screen.blit(score_point_player2, score_point_player2_rect)
+    
     ball_left = pygame.draw.rect(screen, green, (ball_x_left, ball_y_left, 5, 5))
+    ball_right = pygame.draw.rect(screen, blue, (ball_x_right, ball_y_right, 5, 5))
+    
     draw_object(screen, player1.rect)
 
     pygame.display.flip()
